eb479e8776c386554f42ba8b0d8132fe803d45fe
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-02-09 11:40:52 -0500

    Add Gameboard factory function

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -11,3 +11,24 @@ function Player(name, marker) {
         giveWin,
     };
 }
+
+function Gameboard() {
+    const board = []
+
+    for (let i = 0; i < 3; i++) {
+        board[i] = 0;
+        for (let j = 0; j < 3; j++) {
+            board[i][j] = 0;
+        };
+    };
+
+    const markTile = (row, col, marker) => {
+        if (board[row][col] === 0) {
+            board[row][col] = marker;
+        };
+    };
+
+    return {
+        markTile,
+    };
+}
